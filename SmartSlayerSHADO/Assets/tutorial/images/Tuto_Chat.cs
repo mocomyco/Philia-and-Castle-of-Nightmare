8838ba7e9a65ce1c31b129ee1f0f5bb23ddd9a7b
LAPTOP-QN9K7GAL\m0115 <m011534042@edu.teu.ac.jp>
2018-02-06 15:36:25 +0900

    Tutorial進捗

--- SmartSlayerSHADO/Assets/tutorial/images/Tuto_Chat.cs
+++ SmartSlayerSHADO/Assets/tutorial/images/Tuto_Chat.cs
@@ -3,41 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Tuto_Image : MonoBehaviour {
-    public GameObject[] slide;
-    public GameObject[] EngSlide;
+public class Tuto_Chat : MonoBehaviour {
+
+    public string[] nobelText;
+    public Text nobel;
+    // public GameObject[] EngSlide;
     public Image pauseCanvas;
     private int nowSlideNum;
     public Tuto_States tutoStates;
     public int stateNum;
     public AudioSource BGM;
     public AudioSource SE;
-    
+
 
     void Start()
     {
-
         Debug.Log(stateNum + "Start");
         StaticMaster.tutorial = true;
         nowSlideNum = 0;
         if (pauseCanvas.enabled == false) pauseCanvas.enabled = true;
+
+        ///
+        /// ノベルテキストの０番から開始。無ければ読み込まない。
+        ///
+        if (nobelText.Length >0) nobel.text = nobelText[0];
+
+
+
         //if (StaticMaster.Language == "Japanese")
         //{
-            for (int i = 0; i < slide.Length; i++)
-            {
-                if (i == nowSlideNum)
-                {
-                    slide[i].SetActive(true);
-                }
-                else
-                {
-                    slide[i].SetActive(false);
-                }
-            }
-            //for (int i = 0; i < EngSlide.Length; i++)
-            //{
-            //    EngSlide[i].SetActive(false);
-            //}
+
+        //for (int i = 0; i < EngSlide.Length; i++)
+        //{
+        //    EngSlide[i].SetActive(false);
+        //}
         //}
         //else if (StaticMaster.Language == "English")
         //{
@@ -62,28 +61,35 @@ public class Tuto_Image : MonoBehaviour {
         BGM.volume = 0.5f;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        ///
+        ///
         StaticMaster.AnchorNum = 3;
 
         if (Input.GetMouseButtonDown(0))
         {
 
             nowSlideNum += 1;
+
+            if (nowSlideNum >= nobelText.Length)
+            {
+                BGM.volume = 1;
+                StaticMaster.AnchorNum = 1;
+                pauseCanvas.enabled = false;
+                tutoStates.SendMessage("NextContentsNum", stateNum);
+                gameObject.SetActive(false);
+
+            }else nobel.text = nobelText[nowSlideNum];
+
+
+            ///全回数で次へ
+            //if(nowSlideNum>nobelText.Length)
+
             //if (StaticMaster.Language == "Japanese")
             //{
-                for (int i = 0; i < slide.Length; i++)
-                {
-                    if (i == nowSlideNum)
-                    {
-                        slide[i].SetActive(true);
-                    }
-                    else
-                    {
-                        slide[i].SetActive(false);
-                    }
-            }
+
             //}
 
             //else if (StaticMaster.Language == "English")
@@ -100,15 +106,8 @@ public class Tuto_Image : MonoBehaviour {
             //        }
             //    }
             //}
-            if (nowSlideNum >= slide.Length)
-            {
-                BGM.volume = 1;
-                StaticMaster.AnchorNum = 1;
-                pauseCanvas.enabled = false;
-                tutoStates.SendMessage("NextContentsNum", stateNum);
-                gameObject.SetActive(false);
 
-            }
         }
     }
 }
+
